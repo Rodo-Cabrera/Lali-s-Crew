64b07d2b64fa877db50d3dae28eff9005788fafd
Rodo <rodolfocabrera007@gmail.com>
2023-02-12 12:33:35 -0300

    guardando cambios en main del día de hoy

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('nav');
+window.addEventListener("scroll", navShadow);
+
+function navShadow () {
+  if (window.scrollY > 500) {
+    nav.classList.add("bg-dark", "shadow", "opacity-50");
+  } else {
+    nav.classList.remove("bg-dark", "shadow", "opacity-50");
+  }
+}
