4b592ea5f42146c8642a66dbd81e9c6ee3203bda
Matias95Rosales <r.matiasssexe@gmail.com>
2023-02-22 17:32:01 -0300

    22-2--2

--- app/registro.js
+++ app/registro.js
@@ -1,66 +1,9 @@
 
-// const formulario = document.getElementsByName("reg");
-// const nombre = document.getElementsByName("nombre");
-// const email = document.getElementsByName("email");
-// const contrasena = document.getElementsByName("password");
-
-
-// // const registrarUsuario = (e) => {
-// //   e.preventDefault();
-
-
-
-  // const nombreValor = document.reg.nombre.value.toLowerCase();
-  // const emailValor = document.reg.email.value.toLowerCase();
-  // const contrasenaValor = document.reg.password.value.toLowerCase();
-
-//   // Validación del campo nombre
-//   if (nombreValor === "") {
-//     alert("Por favor, ingrese su nombre.");
-//     nombre.focus();
-//     return false;
-//   }
-
-//   // Validación del campo correo electrónico
-//   if (emailValor === "") {
-//     alert("Por favor, ingrese su correo electrónico.");
-//     email.focus();
-//     return false;
-//   } else if (!validarEmail(emailValor)) {
-//     alert("Por favor, ingrese un correo electrónico válido.");
-//     email.focus();
-//     return false;
-//   }
-
-//   // Validación del campo contraseña
-//   if (contrasenaValor === "") {
-//     alert("Por favor, ingrese su contraseña.");
-//     contrasena.focus();
-//     return false;
-//   } else if (contrasenaValor.length < 8) {
-//     alert("La contraseña debe tener al menos 8 caracteres.");
-//     contrasena.focus();
-//     return false;
-//   }
-
-//   // Si todas las validaciones pasan, el formulario se envía
-//   alert("Formulario enviado con éxito!");
-//   e.target.submit();
-// };
-
-// // Función para validar el correo electrónico
-// function validarEmail(email) {
-//   const re = /\S+@\S+.\S+/;
-//   return re.test(email);
-// }
-
 const guardarEnDb = () => {
-  // registrarUsuario();
-  // validarEmail();
+
   fetch('http://localhost:3000/users', {
       method: 'POST',
       body: JSON.stringify({
-
         nombreDeUsuario: document.reg.nombre.value.toLowerCase(),
         email: document.reg.email.value.toLowerCase(),
         contraseña: document.reg.password.value.toLowerCase(),
@@ -81,7 +24,7 @@ const guardarEnDb = () => {
       timer:5000,
     });
     setTimeout(() => {
-      Window.location = `#`;
+      window.location.href = "../index.html";
       }, 10000);
   }
 
